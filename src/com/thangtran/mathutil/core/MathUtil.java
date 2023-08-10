d1a02d926da82eb9aeaa395242dac3353aa23780
warot8112000 <warot8112000@gmail.com>
2023-08-10 16:28:48 +0700

    refactor the main code and green4ever always - fix exception msg

--- src/com/thangtran/mathutil/core/MathUtil.java
+++ src/com/thangtran/mathutil/core/MathUtil.java
@@ -48,7 +48,7 @@ public class MathUtil {
       public static long getFactorial(int n) {
           
         if(n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0..20 please!");
+            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
           
         if(n == 0 || n == 1)
             return 1;
